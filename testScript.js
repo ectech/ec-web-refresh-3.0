018d308e25402d1d722fce842a054efcd01e84bb
Aaron Coplan <afcoplan@verizon.net>
2016-02-16 14:29:34 -0500

    EC-26 - Removed script file from old location as it is now in JS folder

--- testScript.js
+++
@@ -1,7 +0,0 @@
-function flipButton(el){
-	"use strict";
-    var id = $(el).parents(".flip-box").attr('id'); // get the id of the card to flip
-    $(".flip-box:not('div#" + id + "')").removeClass("flip"); // unflip all other boxes first, so that there can only be one flipped at a time
-    
-	document.querySelector("#" + id).classList.toggle("flip");	
-}
